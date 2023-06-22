ea1a00afb095aebe9987f68106fe4567d09144b4
honehone12 <marshalc@hotmail.co.jp>
2023-06-22 15:22:12 +0900

    added logout

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -7,6 +7,17 @@ export default function Home() {
             <h2 className="text-center py-10 text-8xl">
             🤩
             </h2>
+            <form 
+                action="/logout"
+                method="post"    
+            >
+                <button
+                    type="submit"
+                    className="absolute top-8 right-8 rounded-xl bg-yellow-300 font-semibold text-blue-800 px-3 py-2 transition duration-300 ease-in-out hover:bg-yellow-400 hover:-translate-y-1"
+                >
+                    Sign Out
+                </button>
+            </form>
             <div className="w-full justify-center items-center">
                 <nav className="h-10 bg-gray-100 rounded-lg justify-center mx-2 flex space-x-10">
                     <Link 
@@ -23,8 +34,6 @@ export default function Home() {
                     >Claim</Link>
                 </nav>
             </div>
-            
-            
             <Outlet/>
         </Layout>
     );
